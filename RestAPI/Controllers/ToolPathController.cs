930612470f38717c78f9e9830458a525000b03a3
Jestin Stoffel <jestin.stoffel@comercebank.com>
2012-05-29 11:23:39 -0500

    Added an MVC project for the REST API

---
+++ RestAPI/Controllers/ToolPathController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using UI.Presenters;
+
+namespace RestAPI.Controllers
+{
+    public class ToolPathController : Controller
+    {
+        private readonly IToolPathGeneratorPresenter _presenter;
+
+        public ToolPathController(IToolPathGeneratorPresenter presenter)
+        {
+            _presenter = presenter;
+        }
+
+        //
+        // GET: /ToolPath/Generate
+
+        public ActionResult Generate(string stlData)
+        {
+            return Json("");
+        }
+
+    }
+}
